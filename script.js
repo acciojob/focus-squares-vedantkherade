fe3d59d4d17151fdc7fc2626ca6fcaa59451e0fe
vedantkherade <152008775+vedantkherade@users.noreply.github.com>
2025-09-22 11:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square")
+
+squares.forEach((sq) => {
+  sq.addEventListener("mouseover", () => {
+    squares.forEach((s) => s.style.backgroundColor = "#6F4E37"); // all coffee
+    sq.style.backgroundColor = "#E6E6FA"; // hovered one lavender
+  });
+	
+ sq.addEventListener("mouseout", () => {
+    squares.forEach((s) => s.style.backgroundColor = "#E6E6FA"); // reset all lavender
+  });
+})
